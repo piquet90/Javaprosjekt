f2d5a2d707c9240d910c795de4773e0286491c68
Rudi Wei Yu <rudiwyu@gmail.com>
2015-04-16 13:48:44 +0200

    ryddet i Insurance.java

--- Javaprosjekt/src/models/classes/Insurance.java
+++ Javaprosjekt/src/models/classes/Insurance.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package models.classes;
 
 import java.util.Date;
@@ -13,11 +9,23 @@ import java.util.Date;
  */
 public abstract class Insurance {
     
+    
+    
+    
+    private int price;
+    private int coverage;
+    private String conditions;
+    private String other;
+    private Date dateofcreation;
+    
+    
+    
+    
     /**
      * Constructor for Insurance.
      * Creates an Insurance with the required fields
      * 
-     * 
+     * @author Rudi Yu
      * @param price Integer with value of yearly insurance cost
      * @param coverage Integer with value of coverage
      * @param conditions String with terms and conditions of insurance
@@ -32,13 +40,12 @@ public abstract class Insurance {
         this.dateofcreation = dateofcreation;
     }
     
-    private int price;
-    private int coverage;
-    private String conditions;
-    private String other;
-    private Date dateofcreation;
+
 
     /**
+     * Returns yearly price of insurance
+     * 
+     * @author Rudi Yu
      * @return the price
      */
     public int getPrice() {
@@ -46,6 +53,9 @@ public abstract class Insurance {
     }
 
     /**
+     * Sets yearly price of insurance
+     * 
+     * @author Rudi Yu
      * @param price the price to set
      */
     public void setPrice(int price) {
@@ -53,6 +63,8 @@ public abstract class Insurance {
     }
 
     /**
+     * 
+     * @author Rudi Yu
      * @return the coverage
      */
     public int getCoverage() {
@@ -61,7 +73,9 @@ public abstract class Insurance {
 
     /**
      * Sets how much the insurance covers.
+     * 
      * @see "forsikringsbeløp"
+     * @author Rudi Yu
      * @param coverage the coverage to set
      */
     public void setCoverage(int coverage) {
@@ -70,6 +84,8 @@ public abstract class Insurance {
 
     /**
      * Get the conditions String. Conditions contains the terms and conditions of the insurance.
+     * 
+     * @author Rudi Yu
      * @return the conditions
      */
     public String getConditions() {
@@ -78,6 +94,8 @@ public abstract class Insurance {
 
     /**
      * Sets the conditons String. Should containt full insurance-conditions
+     * 
+     * @author Rudi Yu
      * @param conditions The conditions for the insurance
      */
     public void setConditions(String conditions) {
@@ -86,6 +104,8 @@ public abstract class Insurance {
 
     /**
      * Returns "other"-info string. This String should containt information that doesnt fit anywhere else
+     * 
+     * @author Rudi Yu
      * @return "other"-string
      */
     public String getOther() {
@@ -95,6 +115,7 @@ public abstract class Insurance {
     /**
      * Sets other-string. 
      * 
+     * @author Rudi Yu
      * @param other A string containing "other"-info
      */
     public void setOther(String other) {
