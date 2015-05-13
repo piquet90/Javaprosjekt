24fcaf46ebaa4e1d82bca1aa0fe6d557443a8cac
audunb <audun@hbru.no>
2015-05-13 11:37:02 +0200

    div

---
+++ Javaprosjekt/src/views/StatisticsView.java
@@ -0,0 +1,38 @@
+/*
+ * Made by
+ * Rudi Yu s231776
+ * Audun Brustad s236341
+ */
+package views;
+
+import CustomSwing.CustomPanel;
+
+/**
+ *
+ * @author Audun
+ */
+public class StatisticsView extends CustomPanel {
+    
+    /*
+    Inntekter og utgifter:
+    selskapets totale utbetaling av erstatninger i løpet av et år
+    selskapets totale utbetaling av erstatninger for en gitt forsikringstype i løpet av et år
+    selskapets utbetaling til en gitt forsikringskunde i løpet av kundeforholdet
+    selskapets totale forsikringspremieinntekter i løpet av et år
+    selskapets totale forsikringspremieinntekter for en gitt forsikringstype i løpet av et år
+    selskapets forsikringspremieinntekter på en gitt forsikringskunde i løpet av kundeforholdet
+    
+    Statistikk:
+    øking/minking av antall skademeldinger innenfor en gitt tidsperiode
+    øking/minking av skademeldinger av en bestemt type innenfor en gitt tidsperiode
+    øking/minking av de totale erstatningskostnadene
+    øking/minking av erstatningskostnadene for en gitt skadetype innenfor en gitt tidsperiode
+    type forsikringer rangert etter antall
+    */
+    
+    public StatisticsView()
+    {
+        
+    }
+    
+}
