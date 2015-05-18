fa8f407141f2d0576d158df1c5c7e063eba23b28
audunb <audun@hbru.no>
2015-05-18 15:47:23 +0200

    div + bugs

--- Javaprosjekt/src/Test/ViewTest2.java
+++ Javaprosjekt/src/Test/ViewTest2.java
@@ -8,6 +8,7 @@ package Test;
 import CustomSwing.CustomPanel;
 import javax.swing.JOptionPane;
 import views.StatisticsView;
+import views.registrations.NewReportPanel;
 
 
 /**
@@ -20,10 +21,13 @@ public class ViewTest2 {
     public static void main(String[] args) {
         
         
-        StatisticsView s = new StatisticsView();
-        s.setInsurances(15, 3, 38, 3237, 18);
+        /*StatisticsView s = new StatisticsView();
+        s.setInsurances(15, 3, 38, 3237, 18);*/
+        
+        
+        NewReportPanel r = new NewReportPanel();
         CustomPanel p = new CustomPanel();
-        p.add(s);
+        p.add(r);
         
         JOptionPane.showMessageDialog(null, p);       
     }
